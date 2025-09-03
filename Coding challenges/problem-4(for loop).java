8e567346fa08d9fac021cfb4afefcbd3e4925638
AbinayaSenthil06 <abinayasenthil0614@gmail.com>
2025-09-03 16:20:20 +0530

    problem-4(for loop).java

--- Coding challenges/problem-4(for loop).java
+++ Coding challenges/problem-4(for loop).java
@@ -25,3 +25,71 @@ Number 2
 8
 9
 10
+
+// print even number between 1 to 10 Using for loop and if else
+import java.util.Scanner;
+class forloop{
+    public static void main(String args[])
+    {
+    
+       for(int i=1; i<=10; i=i+1)
+       if(i%2==0)
+       System.out.println("Even number:"+i);
+    }
+}
+
+//output:
+Even number:2
+Even number:4
+Even number:6
+Even number:8
+Even number:10
+
+// count the number of odd number from 1 to 10
+import java.util.Scanner;
+class forloop{
+    public static void main(String args[])
+    {
+       int oddcount = 0;
+       for(int i=1; i<=10; i=i+1)
+       {
+        if(i%2==0)
+       {
+         oddcount= oddcount+1;
+       }
+    
+       }
+       System.out.print(oddcount);
+     
+    }
+}
+// output:
+5
+
+// print the number which are divisible by both 3 and 5 . range 1 to 100.
+
+class forloop{
+    public static void main(String args[])
+    {
+        int count = 0;
+        for(int i=1;i<=100;i=i+1)
+        {
+            if(i%3==0 && i%5==0)
+            {
+                count = count+1;
+                System.out.println(i);
+            }
+        }
+
+        System.out.println("count is:"+count);
+    }
+}
+
+// output:
+15
+30
+45
+60
+75
+90
+count is:6
