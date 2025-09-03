39a1fbff9d018a3573a7a39606fae9cc4f5840ad
AbinayaSenthil06 <abinayasenthil0614@gmail.com>
2025-09-03 12:02:32 +0530

    else if.java

---
+++ Conditional statement/else if.java
@@ -0,0 +1,21 @@
+class main{
+    public static void main(String args[])
+    {
+        int score = 62;
+        if(score>35 && score<60)
+        {
+            System.out.println("Video Game");
+        }
+        else if(score>60 && score<90)
+        {
+            System.out.println("Iphone");
+        }
+        else if(score>90)
+        {
+            System.out.print("Macbook pro");
+        }
+    }
+}
+
+output:
+Iphone
