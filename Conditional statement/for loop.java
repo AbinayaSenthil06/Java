5a8dc15aba5de2ab9718de24be5e8acf969bbbb2
AbinayaSenthil06 <abinayasenthil0614@gmail.com>
2025-09-03 15:32:52 +0530

    for loop.java

---
+++ Conditional statement/for loop.java
@@ -0,0 +1,45 @@
+// For loop:  A for loop is a control flow statement used to execute a block of code repeatedly a specified number of times.
+        
+// write a program to print the number from 1 to 10
+class forloop{
+    public static void main(String args[])
+    {
+       for(int i=1; i<=10; i=i+1) // Increment
+        
+        System.out.println(i);
+    }
+}
+
+// Output:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+// write a program to print the number from 1 to 10
+class forloop{
+    public static void main(String args[])
+    {
+       for(int i=10; i>=1; i=i-1)  // Decrement
+        
+        System.out.println(i);
+    }
+}
+
+// output:
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
