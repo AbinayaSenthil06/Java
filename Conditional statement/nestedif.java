524c21442d57016af71741d4ba99c039e8716b8f
AbinayaSenthil06 <abinayasenthil0614@gmail.com>
2025-09-03 12:11:33 +0530

    nestedif.java

---
+++ Conditional statement/nestedif.java
@@ -0,0 +1,27 @@
+class main{
+    public static void main(String args[])
+    {
+      boolean kfc = true;
+      boolean chicken = true;
+      boolean pepsi = true;
+      if(kfc)
+      {
+        System.out.println("Enter into kfc");
+
+        if(chicken)
+        {
+            System.out.println("Eating chicken");
+
+        if(pepsi)
+        {
+            System.out.print("Drinking pepsi");
+        }
+        }
+      }
+    }
+}
+
+output:
+Enter into kfc
+Eating chicken
+Drinking pepsi
